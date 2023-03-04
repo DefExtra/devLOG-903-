c9fbcf5c752cf7b2005d762a77cbca44bc81ace8
yatrfvznking <115359660+yatrfvznking@users.noreply.github.com>
2023-03-04 05:56:27 +0300

    ProYK

--- dashboard.config.js
+++ dashboard.config.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     owners: ["", ""],
-    clientID: "",
-    clientSECRET: "",
+    clientID: "1027835871443025972",
+    clientSECRET: "6KfpHf78zc_6sG8-Z25Rs8ujBgYi_yfE",
     domain: "http://localhost:3030",
     port: 3030
 };
